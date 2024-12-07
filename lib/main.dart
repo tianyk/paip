538968e17c653b4a6a02d19b881102119b6979bb
yongketian@gmail.com <tyk>
2024-12-07 21:14:38 +0800

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Directionality(
+      child: Directionality(
         textDirection: TextDirection.ltr,
         child: DefaultTextStyle(
-          style: TextStyle(color: Colors.black),
+          style: const TextStyle(color: Colors.black),
           child: Column(
             children: [
-              ImageSection(image: 'assets/images/lake.jpg'),
-              TitleSection(),
+              const ImageSection(image: 'assets/images/lake.jpg'),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(children: [
+                    const TitleSection(),
+                    const ButtonSection(),
+                    Container(
+                      margin: const EdgeInsets.only(top: 16),
+                      child: const TextSection(
+                        text: 'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                            'Bernese Alps. Situated 1,578 meters above sea level, it '
+                            'is one of the larger Alpine Lakes. A gondola ride from '
+                            'Kandersteg, followed by a half-hour walk through pastures '
+                            'and pine forest, leads you to the lake, which warms to 20 '
+                            'degrees Celsius in the summer. Activities enjoyed here '
+                            'include rowing, and riding the summer toboggan run.',
+                      ),
+                    ),
+                  ]))
             ],
           ),
         ),
@@ -46,29 +63,75 @@ class TitleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "小明的餐厅",
+              style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w600),
+            ),
+            Text("一家好吃的餐厅", style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w400)),
+          ],
+        ),
+        Row(
+          children: [
+            Icon(Icons.star, color: Colors.red),
+            Text("41"),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DefaultTextStyle(
+      style: TextStyle(color: Colors.purple, fontSize: 18, fontWeight: FontWeight.w400),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "小明的餐厅",
-                style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w600),
-              ),
-              Text("一家好吃的餐厅", style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w400)),
-            ],
-          ),
-          Row(
-            children: [
-              Icon(Icons.star, color: Colors.red),
-              Text("41"),
-            ],
-          ),
+          IconText(icon: Icons.call, text: "Call"),
+          SizedBox(width: 24),
+          IconText(icon: Icons.near_me, text: "Near me"),
+          SizedBox(width: 24),
+          IconText(icon: Icons.share, text: "Share"),
         ],
       ),
     );
+  }
+}
+
+class IconText extends StatelessWidget {
+  const IconText({super.key, required this.icon, required this.text});
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, size: 48, color: Colors.purple),
+        Text(text, style: const TextStyle(color: Colors.purple, fontSize: 18, fontWeight: FontWeight.w400)),
+      ],
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text, style: const TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w400));
   }
 }
